3b5992e02f13933ff901d2005480a376e9139169
vjcitn <stvjc@channing.harvard.edu>
2023-09-07 05:04:29 -0400

    dropping the gzipped gwascat representation

--- inst/assoc/ebicat_rsqlite.R
+++ inst/assoc/ebicat_rsqlite.R
@@ -1,8 +1,11 @@
 
+# this is one way to produce a comprehensive SQLite representation
+# of the GWAS catalog.  you will still have to deal with the fact
+# that many CHR_POS are non-numeric 
 library(RSQLite)
 library(dplyr)
 library(readr) # necessary because of embedded EOF
-tab = suppressWarnings(read_tsv("ebi_gwcat_alternative_20230901.gz"))
+tab = suppressWarnings(read_tsv([path to alternative tsv]))
 mycon = dbConnect(SQLite(), ":memory:")
 assoctab = dbWriteTable(mycon, "assoc", tab)
 tbl(mycon, "assoc")
